86e6728dd61688030d096703276010f2359f891c
caralinebruz <caraline.bruzinski@gmail.com>
2022-11-27 19:30:40 -0500

    working for basic naive bayes simple data

--- NaiveBayes.py
+++ NaiveBayes.py
@@ -28,6 +28,10 @@ class NaiveBayes:
 
 		self.priors = {}
 
+		self.actual = {}
+		self.predicted = {}
+		self.correct_prediction = {}
+
 
 	def clean(self,text):
 		''' Prepares a file for use. Validates row length
@@ -92,18 +96,6 @@ class NaiveBayes:
 				a single integer 
 		'''
 		return len(self.train_df.index)
-
-	# def make_total_rows_by_class(self):
-	# 	''' *Returns:
-	# 			A dictionary of total rows by class
-	# 	'''
-	# 	for label in self.labels:
-
-	# 		# get the number of rows with each class
-	# 		num_rows_of_class = len(self.train_df[self.train_df.Class == label])
-	# 		print("label:%s rows:%s" % (label, num_rows_of_class))
-
-	# 		self.totals_by_class[str(label)] = num_rows_of_class
 
 
 	def print_likelihoods(self):
@@ -192,11 +184,6 @@ class NaiveBayes:
 
 		# get the total (for normalization)
 		self.total_rows = self.get_total_rows()
-		# self.make_total_rows_by_class()
-
-		# print("total rows by class:")
-		# for k,v in self.total_rows_by_class.items():
-		# 	print(k,v)
 
 		# get the priors
 		self.make_prior_probabilities()
@@ -205,111 +192,150 @@ class NaiveBayes:
 		self.make_likelihoods()
 
 
-		''' likelihoods
-			for each class, make a dictionary for their conditional probabilities
-				each dictionary contains keys as predictor variable names
-										 values as predictor variable values (dict)
-										 		values of this dict will be frequency counts
+	def _argmax(self, h_x):
+		''' Sifts through args calculated
+			*Returns:
+				prediction of class based on argmax
 		'''
+		largest = float('-inf')
+		selected_class = None
+
+		for label in h_x.keys():
+
+			if h_x[label] > largest:
+				largest = h_x[label]
+				selected_class = label
+
+		return selected_class
+
 
 	def classify(self, item):
 		''' Attempt to classify the item 
 		'''
 		items_class = item[-1]
 		items_data = item[:-1]
-
 		data = items_data.copy()
 
-		# data_vec = list(map(int, data_vec))
-
-		# create the equation h(x) for each potential class (my case, T/F)
+		arg = {}
 
 		for label in self.labels:
 
-
 			h_x = self.priors[label]
 
-			print("h_x prior = %s" % h_x)
-
-
-			# each predictor has form:
-			#
-			# (num rows containing val, cond on class) / (total_by_class)
-			#
-
+			# print("h_x prior = %s" % h_x)
+			print("P(C=%s) = [%s]" % (label, h_x))
 
 			for i in range(len(data)):
 
-				# value = data.pop()
+				# extract the test value of the current predictor
 				value = data[i]
-
-				num_rows_containing_val_c_class = 0
-
-				# test = self.likelihoods[label][str(i)][str(value)]
-
-				# age = ages.get('Jim',0)
+				
+				# get look up the frequency, if available
 				test = self.likelihoods[label][str(i)].get(str(value))
 
-				print("TEST VAL %s" % test)
-
-
-
+				# if it is available, populate it
 				if value in self.likelihoods[label][str(i)].keys():
-					print("in there")
 																	 # class  # col  # unique val
 					num_rows_containing_val_c_class = self.likelihoods[label][str(i)][str(value)]
 				
 				else:
-					print("missing")
+					# otherwise use default 0 numerator
+					num_rows_containing_val_c_class = 0
 
 				numerator = num_rows_containing_val_c_class
 				denominator = self.total_rows_by_class[label]
 
-				print("\tmult %s / %s" % (numerator, denominator))
+				# print("\tmult %s / %s" % (numerator, denominator))
+				print("P(%s=%s | C=%s) %s / %s" % (i, value, label, numerator, denominator))
+
 				h_x = h_x * (numerator / denominator)
 
 
+			# print("\th_x:%s" % h_x)
+			print("NB(C=%s) = %s" % (label, h_x))
 
-			print("\th_x:%s" % h_x)
+			# stash the computation for argmax
+			arg[label] = h_x
 
-			# then, divide by total rows
-			print("dividing by total rows: %s" % self.total_rows)
-			h_x = h_x / self.total_rows
+		return self._argmax(arg)
 
-			print("\th_x:%s" % h_x)
 
+	def initialize_precision_recall(self):
+		''' Makes dictionaries for each label
+			for tracking prediction scores
+		'''
+		actual = {}
+		predicted = {}
+		correct_prediction = {}
+		for label in self.labels:
+			actual[label] = 0
+			predicted[label] = 0
+			correct_prediction[label] = 0
+
+		self.actual = actual
+		self.predicted = predicted
+		self.correct_prediction = correct_prediction
+
+
+	def print_precision_recall(self):
+		''' Prints performance of classifier
+		'''
+		if self.verbose:
+			print("actuals")
+			for k,v in self.actual.items():
+				print(k,v)
+			print("predicted")
+			for k,v in self.predicted.items():
+				print(k,v)
+			print("correct")
+			for k,v in self.correct_prediction.items():
+				print(k,v)
+
+
+		for label in self.labels:
+			print("Label=%s Precision=%s/%s Recall=%s/%s" % (label, self.correct_prediction[label], self.predicted[label], self.correct_prediction[label], self.actual[label]))
 
 
 
 	def test(self, data):
-		''' Primary logic
+		''' 
 		'''
 		clean = self.clean(data)
 		df = pd.DataFrame(data=clean)
 		df.columns = [*df.columns[:-1], 'Class']
 
+		self.initialize_precision_recall()
+
 		self.test_df = df
 		self.test_data = clean
 
-
-
-		# # # get the unique values "actuals" in test set
-		# self.prepare_labels()
-
-		print("starting test")
-		for row in self.test_data[:1]:
+		for row in self.test_data:
 
 			print(row)
+			actual_class = row[-1]
 
 			if len(row) != self.numcols:
 				print("test row has different N-dimensions from train set. abort")
 				sys.exit(1)
 
-			self.classify(row)
+			# make a prediction
+			prediction = self.classify(row)
+
+			if self.verbose:
+				if prediction != actual_class:
+					print("fail: got '%s' != want '%s'" % (prediction, actual_class))
+				else:
+					print("match: '%s'" % prediction)
 
 
+			self.predicted[prediction] += 1
+			self.actual[actual_class] += 1
+
+			if actual_class == prediction:
+				self.correct_prediction[actual_class] += 1
 
 
+		self.print_precision_recall()
 
 
 
